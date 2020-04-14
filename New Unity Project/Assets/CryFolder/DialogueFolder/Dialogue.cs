d5e433b46159096455babf71e36ae0a402cb6d80
hanglch <46513241+hanglch@users.noreply.github.com>
2020-04-14 23:33:26 +0800

    version 0.15 First Draft of Dialogue System
    One direction dialogue system done
    Planning to insert option to the system

---
+++ New Unity Project/Assets/CryFolder/DialogueFolder/Dialogue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue: MonoBehaviour
+{
+    public string[] sentences;
+    public string NPC_name;
+}
---
+++ New Unity Project/Assets/CryFolder/DialogueFolder/DialogueManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text DialogueText;
+    public string fulltext;
+    public string currenttext;
+
+    [SerializeField] public Queue<string> sentences = new Queue<string>();
+    public void DialogueStart(Dialogue dialogue)
+    {
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        NextSentence();
+    }
+
+    public void NextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            Debug.Log("end dialogue");
+        }
+        fulltext = sentences.Dequeue();
+        StartCoroutine(DisplayDialogue());
+    }
+
+    IEnumerator DisplayDialogue() {
+        for (int i = 0; i < fulltext.Length; i ++)
+        {
+            currenttext = fulltext.Substring(0, i+1);
+            DialogueText.text = currenttext;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
