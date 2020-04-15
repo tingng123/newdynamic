155d914bcb3bed5fbce65c37ae1718a10ae9a0f6
hanglch <46513241+hanglch@users.noreply.github.com>
2020-04-15 20:27:09 +0800

    0.16

--- New Unity Project/Assets/CryFolder/DialogueFolder/Dialogue.cs
+++ New Unity Project/Assets/CryFolder/DialogueFolder/Dialogue.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Dialogue: MonoBehaviour
 {
-    public string[] sentences;
+    public int set;
     public string NPC_name;
+    public string[] sentences;
+    public bool optionCheck;
+    public string[] options;
 }
--- New Unity Project/Assets/CryFolder/DialogueFolder/DialogueManager.cs
+++ New Unity Project/Assets/CryFolder/DialogueFolder/DialogueManager.cs
@@ -8,34 +8,97 @@ public class DialogueManager : MonoBehaviour
     public Text DialogueText;
     public string fulltext;
     public string currenttext;
+    public Dialogue[] DialogueSet;
+    public int DialogueSetOrder;
+    public bool FirstDialogue;
+    public bool displaying;
+    public Dialogue currentDialogue;
+    public GameObject optionpanel;
+    public GameObject[] optionbuttons;
+    public Text[] optiontext;
 
     [SerializeField] public Queue<string> sentences = new Queue<string>();
     public void DialogueStart(Dialogue dialogue)
     {
+        if (FirstDialogue == true)
+        {
+            DialogueSetOrder = 0;
+            FirstDialogue = false;
+        }
+
+        currentDialogue = dialogue;
         sentences.Clear();
+        //putting sentence inside dialogue class into the queue
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
         NextSentence();
+        DialogueSet = dialogue.gameObject.GetComponents<Dialogue>();
     }
 
     public void NextSentence()
     {
-        if(sentences.Count == 0)
+        //Clearing of the sentence;
+        if (sentences.Count == 0)
         {
-            Debug.Log("end dialogue");
+            if (currentDialogue.optionCheck == true)
+            {
+                for (int i = 0; i < currentDialogue.options.Length; i++)
+                {
+                    Debug.Log("option " + i + " is " + currentDialogue.options[i]);
+                    optionbuttons[i] = optionpanel.transform.GetChild(i).gameObject;
+                    optiontext[i] = optionbuttons[i].transform.GetChild(0).gameObject.GetComponent<Text>();
+                    optiontext[i].text = currentDialogue.options[i];
+                }
+                optionpanel.SetActive(true);
+            }
+            else
+            {
+                //reset
+                FirstDialogue = true;
+                currenttext = "";
+                DialogueText.text = currenttext;
+            }
         }
-        fulltext = sentences.Dequeue();
-        StartCoroutine(DisplayDialogue());
+
+        // Quickly show the whole sentence
+        if (displaying == true)
+        {
+            displaying = false;
+            currenttext = fulltext;
+            DialogueText.text = currenttext;
+
+        }
+        //normal process, display next sentence
+        else
+        {
+            fulltext = sentences.Dequeue();
+            StartCoroutine(DisplayDialogue());
+        }
     }
 
     IEnumerator DisplayDialogue() {
+        displaying = true;
+        //keyboard typing display style
         for (int i = 0; i < fulltext.Length; i ++)
         {
-            currenttext = fulltext.Substring(0, i+1);
-            DialogueText.text = currenttext;
-            yield return new WaitForSeconds(0.1f);
+            if (displaying == true)
+            {
+                currenttext = fulltext.Substring(0, i + 1);
+                DialogueText.text = currenttext;
+                yield return new WaitForSeconds(0.3f);
+            }
         }
+        displaying = false;
     }
+
+    //called when option button is onclicked
+    public void OptionDetermine(int chosennumber)
+    {
+        DialogueSetOrder += chosennumber;
+        DialogueStart(DialogueSet[DialogueSetOrder]);
+        optionpanel.SetActive(false);
+    }
+
 }
