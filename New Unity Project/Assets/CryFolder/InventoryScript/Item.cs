5a87b7c3977628bd798b98516afa691664ea8383
hanglch <46513241+hanglch@users.noreply.github.com>
2020-04-18 23:49:14 +0800

    Merge

---
+++ New Unity Project/Assets/CryFolder/InventoryScript/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour
+{
+    public int ID;
+    public string ItemName;
+    public Sprite icon;
+    public int maxamount;
+    public string type;
+
+    
+
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    //    Physics.IgnoreCollision(collision.collider2D, GetComponent<Collider>());
+        if (collision.gameObject.tag == "Player" )
+        {
+            collision.gameObject.GetComponent<PlayerInventory>().additem(this.GetComponent<Item>().ID);
+            Destroy(this.gameObject);
+        }
+    }
+}
